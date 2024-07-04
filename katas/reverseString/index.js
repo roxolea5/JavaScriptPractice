7d06f69c11a1ea574ef077ca572d0e1a10cf72e5
roxolea5 <roxana.olea@gmail.com>
2024-07-04 12:53:39 -0600

    reverse string array helper solution 3

--- katas/reverseString/index.js
+++ katas/reverseString/index.js
@@ -7,13 +7,16 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  let reversed = "";
-  for (let character of str){
-    reversed = character + reversed;
-  }
-  return reversed
+  return str.split("").reduce((rev, char) => char + rev, "");
 }
 
+// function reverse(str) {
+//   let reversed = "";
+//   for (let character of str){
+//     reversed = character + reversed;
+//   }
+//   return reversed
+// }
 
 // function reverse(str) {
 //   return str.split("").reverse().join("");
